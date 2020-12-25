9a9ab7efde5199bc0a493b62f46cc7acd428d0b6
Michael <mschuff0881@gmail.com>
2020-12-24 21:49:30 -0800

    switched to using a single texture for rendering when doing use_colors

--- SquareDance/Assets/Scripts/main.cs
+++ SquareDance/Assets/Scripts/main.cs
@@ -7,6 +7,7 @@ public class main : MonoBehaviour
     public GameObject[] tiles;
     public GameObject point;
     public GameObject fill_button;
+    public GameObject plane;
 
     //array holding the tiles that point to the dots in the tilted square
     //0 = none
@@ -81,10 +82,14 @@ public class main : MonoBehaviour
             }
         }
         empty = new List<Vector2Int>();
+        redraw_tiles();
     }
 
     public void iterate() {
-        gameObject.transform.position = gameObject.transform.position + new Vector3(0, 2, 0);
+        if (!use_colors)
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0, 2, 0);
+        }
         iteration++;
         int[,,] n_points = new int[iteration, iteration, 2];
         for (int y = 0; y < iteration - 1; y++) {
@@ -172,67 +177,118 @@ public class main : MonoBehaviour
         {
             fill();
         }
+        redraw_tiles();
     }
 
     void redraw_tiles()
     {
-        var clones = GameObject.FindGameObjectsWithTag("clone");
-        foreach (var clone in clones) {
-            Destroy(clone);
-        }
-
-        float t = (points.GetLength(0) - 1) / 2f;
+        
+        float t = (points.GetLength(0) - 1) / 2f; //center
         if (use_colors)
         {
+            Texture2D texture = new Texture2D((iteration-1)*2 + 10, (iteration - 1)*2 + 10);
+            print(texture.GetPixel(0, 0).r.ToString() +  texture.GetPixel(0, 0).g.ToString() + texture.GetPixel(0, 0).b.ToString());
             for (int y = 0; y < points.GetLength(0); y++)
             {
                 for (int x = 0; x < points.GetLength(1); x++)
                 {
-                    int ind = points[y, x, 0] + 4;
-
-                    if (ind != 4)
-                    {
-                        Instantiate(tiles[ind], new Vector3(x + y - 2 * t, 0, x - y), tiles[ind].transform.rotation);
-                    }
-                    ind = points[y,x,1] + 4;
-                    if (ind != 4)
-                    {
-                        Instantiate(tiles[ind], new Vector3(x + y - 2 * t, 0, x - y), tiles[ind].transform.rotation);
+                    int newx = x + y;
+                    int newy = (int) (x - y + 2 * t);
+                    for (int i = 0; i < 2; i++) {
+                        switch (points[y, x, i])
+                        {
+                            case 1:
+                                {
+                                    texture.SetPixel(5 + newx, 5 + newy - 1, new Color(0f, 1f, 0.18f));
+                                    texture.SetPixel(5 + newx - 1, 5 + newy - 1, new Color(0f, 1f, 0.18f));
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    texture.SetPixel(5 + newx, 5 + newy, new Color(0.98f, 0.86f, 0.27f));
+                                    texture.SetPixel(5 + newx, 5 + newy - 1, new Color(0.98f, 0.86f, 0.27f));
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    texture.SetPixel(5 + newx, 5 + newy, new Color(0.42f, 0.56f, 0.99f));
+                                    texture.SetPixel(5 + newx - 1, 5 + newy, new Color(0.42f, 0.56f, 0.99f));
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    texture.SetPixel(5 + newx - 1, 5 + newy, new Color(0.97f, 0.06f, 0f));
+                                    texture.SetPixel(5 + newx - 1, 5 + newy - 1, new Color(0.97f, 0.06f, 0f));
+                                    break;
+                                }
+                        }
                     }
                 }
             }
+
+            for (int i = 0; i < empty.Count; i++)
+            {
+                int newx = 1 + empty[i].x + empty[i].y;
+                int newy = (int)(empty[i].x - empty[i].y + 2 * t);
+                texture.SetPixel(5 + newx, 5 + newy, new Color(0f, 0.55f, 0.16f));
+                texture.SetPixel(5 + newx - 1, 5 + newy, new Color(0f, 0.55f, 0.16f));
+                texture.SetPixel(5 + newx, 5 + newy - 1, new Color(0f, 0.55f, 0.16f));
+                texture.SetPixel(5 + newx - 1, 5 + newy - 1, new Color(0f, 0.55f, 0.16f));
+            }
+            texture.filterMode = FilterMode.Point;
+            texture.Apply();
+
+            Material material = new Material(Shader.Find("Diffuse"));
+            material.mainTexture = texture;
+            plane.GetComponent<Renderer>().material = material;
+
+
+            
         } else
         {
+            var clones = GameObject.FindGameObjectsWithTag("clone");
+            foreach (var clone in clones)
+            {
+                Destroy(clone);
+            }
             for (int y = 0; y < points.GetLength(0); y++)
             {
                 for (int x = 0; x < points.GetLength(1); x++)
                 {
-                    int ind = points[y,x,0];
+                    int ind = points[y, x, 0];
                     Instantiate(point, new Vector3(x + y - 2 * t, 0, x - y), point.transform.rotation);
                     if (ind != 0)
                     {
                         Instantiate(tiles[ind], new Vector3(x + y - 2 * t, 0, x - y), tiles[ind].transform.rotation);
                     }
-                    ind = points[y,x,1];
+                    ind = points[y, x, 1];
                     if (ind != 0)
                     {
                         Instantiate(tiles[ind], new Vector3(x + y - 2 * t, 0, x - y), tiles[ind].transform.rotation);
                     }
                 }
             }
+            for (int i = 0; i < empty.Count; i++)
+            {
+                Instantiate(tiles[0], new Vector3(1f + empty[i].x + empty[i].y - 2 * t, 0, empty[i].x - empty[i].y), tiles[0].transform.rotation);
+            }
         }
-        for (int i = 0; i < empty.Count; i++)
-        {
-            Instantiate(tiles[0], new Vector3(1f + empty[i].x + empty[i].y - 2 * t, 0, empty[i].x - empty[i].y), tiles[0].transform.rotation);
-        }
+
+        
     }
 
     void Start()
     {
+        if (!use_colors)
+        {
+            plane.SetActive(false);
+        }
+
         if (auto_fill)
         {
             fill_button.SetActive(false);
         }
+
         if (rand())
         {
             points = new int[,,]
@@ -248,13 +304,13 @@ public class main : MonoBehaviour
                 { { 0, 0 }, { 4, 0 } }
             };
         }
+        redraw_tiles();
 
     }
 
     
     void Update()
     {
-        redraw_tiles();
         
     }
     void print_points()
